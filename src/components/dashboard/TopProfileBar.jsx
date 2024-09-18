e5a5ae4f2a2273b179b9232b2859aa7dc7b4ae75
martynhasabraincell <martyntok@gmail.com>
2024-09-18 18:18:34 +0800

    - added images and icons - loaded images and icons into top profile bar and nav bar

--- src/components/dashboard/TopProfileBar.jsx
+++ src/components/dashboard/TopProfileBar.jsx
@@ -1,7 +1,9 @@
 function TopProfileBar() {
     return (
-        <div className="w-[100%] h-[100%]">
-            <h1>Top Profile Bar Goes Here</h1>
+        <div className="w-[100%] h-[100%]" >
+            <img src="src/assets/images/settings_icon.png" alt="Settings icon"/>
+            <img src="src/assets/images/manager_profile.jpg" alt="Your profile picture"/>
+            <h1>Your Name</h1>
         </div>
     )
 }
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -2,6 +2,8 @@ function NavBar() {
     return (
         <div className="w-[100%] h-[100%]">
             <h1>NavBar Here</h1>
+            <img src="src/assets/images/all-in-one_logo.png" alt="Company logo"/>
+            
         </div>
     )
 }
