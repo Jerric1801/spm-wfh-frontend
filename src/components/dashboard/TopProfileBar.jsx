5759b012141ac75042bfb730605d8b80e9dce354
martynhasabraincell <martyntok@gmail.com>
2024-09-20 21:25:11 +0800

    updated layout of components

--- src/components/dashboard/TopProfileBar.jsx
+++ src/components/dashboard/TopProfileBar.jsx
@@ -1,9 +1,9 @@
 function TopProfileBar() {
     return (
-        <div className="w-[100%] h-[100%]" >
-            <img src="src/assets/images/settings_icon.png" alt="Settings icon"/>
-            <img src="src/assets/images/manager_profile.jpg" alt="Your profile picture"/>
-            <h1>Your Name</h1>
+        <div className="w-[100%] h-[100%]" style={{ display: 'flex', flexDirection:'row', justifyContent:'flex-end', }}>
+            <img src="src/assets/images/settings_icon.png" alt="Settings icon" style={{ alignSelf:'flex-end',marginBlock: 'auto', marginRight:'20px', width:'2%'}}/>
+            <img src="src/assets/images/manager_profile.jpg" alt="Your profile picture"  style={{ padding:'10px', marginRight:'10px'}}/>
+            <h1 style={{marginBlock: 'auto', marginRight:'20px'}} className="text-tag-grey-dark"><b>Yu Junjie</b></h1>
         </div>
     )
 }
