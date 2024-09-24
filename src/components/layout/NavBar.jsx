c845893a5a24d7674e2ac8583d12f5829721dd04
martynhasabraincell <martyntok@gmail.com>
2024-09-24 09:38:13 +0800

    fixed issue of margin adding height to the left nav bar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,7 +1,7 @@
 function NavBar() {
     return (
-        <div className="w-[100%] h-[100%]" >
-            <img src="src/assets/images/all-in-one_logo.png" alt="Company logo" style={{ width: '40%',  margin: 'auto' , marginTop: '20px'}}/>
+        <div className="w-[100%] h-[100%]" style={{paddingTop:'20px'}}>
+            <img src="src/assets/images/all-in-one_logo.png" alt="Company logo" style={{ width: '40%',  margin: 'auto' }}/>
             <img src="src/assets/images/request-page-icon.png" alt="Request page icon" style={{ width: '40%', margin: 'auto', marginTop: '40px', padding: '10px'}}/>
             
         </div>
