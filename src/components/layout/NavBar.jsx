5b6e20ddc0b00d8278361e1ad00d9b8dda0ec888
martynhasabraincell <martyntok@gmail.com>
2024-09-24 19:52:28 +0800

    imported assets

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,8 +1,11 @@
+import CompanyLogo from "../../assets/images/all-in-one_logo.png";
+import RequestPageIcon from "../../assets/images/request-page-icon.png";
+
 function NavBar() {
     return (
         <div className="w-[100%] h-[100%]" style={{paddingTop:'20px'}}>
-            <img src="./assets/images/all-in-one_logo.png" alt="Company logo" style={{ width: '40%',  margin: 'auto' }}/>
-            <img src="./assets/images/request-page-icon.png" alt="Request page icon" style={{ width: '40%', margin: 'auto', marginTop: '40px', padding: '10px'}}/>
+            <img src={CompanyLogo} alt="Company logo" style={{ width: '40%',  margin: 'auto' }}/>
+            <img src={RequestPageIcon} alt="Request page icon" style={{ width: '40%', margin: 'auto', marginTop: '40px', padding: '10px'}}/>
             
         </div>
     )
