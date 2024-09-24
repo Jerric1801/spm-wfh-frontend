3c214861a66ae4f11ae18c93d776b25cf4cdfcc6
martynhasabraincell <martyntok@gmail.com>
2024-09-24 09:47:54 +0800

    updated image paths to be relative

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,8 +1,8 @@
 function NavBar() {
     return (
         <div className="w-[100%] h-[100%]" style={{paddingTop:'20px'}}>
-            <img src="src/assets/images/all-in-one_logo.png" alt="Company logo" style={{ width: '40%',  margin: 'auto' }}/>
-            <img src="src/assets/images/request-page-icon.png" alt="Request page icon" style={{ width: '40%', margin: 'auto', marginTop: '40px', padding: '10px'}}/>
+            <img src="../../src/assets/images/all-in-one_logo.png" alt="Company logo" style={{ width: '40%',  margin: 'auto' }}/>
+            <img src="../../src/assets/images/request-page-icon.png" alt="Request page icon" style={{ width: '40%', margin: 'auto', marginTop: '40px', padding: '10px'}}/>
             
         </div>
     )
