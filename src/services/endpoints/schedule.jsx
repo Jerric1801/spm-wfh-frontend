16f716db46831ba58d98e0cf207977584fc56fcd
Jerric1801 <Jerric1801@gmail.com>
2024-10-01 10:10:48 +0800

    ⚙️ created services for schedule

---
+++ src/services/endpoints/schedule.jsx
@@ -0,0 +1,21 @@
+import api from '../api';
+
+const getCalendar = (month, token) => {
+    //backend fetches user data
+    //checks uses access control
+    //get personal schedule based on month
+    //get available departments and teams & overall statistics
+}
+
+const getTeamSchedule = (dept, team, startDate, endDate, token) => {
+    //called each time a range is selected or a new department or team is selected on UI
+    //fetch user data
+    // checks access control
+    // fetch WFH status based on 
+}
+
+export 
+{
+    getCalendar,
+    getTeamSchedule
+}
